e495d75c45cc28c19977716e0c414fe3ebcb2899
Lucas Lira <lucasrocha446@gmail.com>
2022-06-16 12:13:29 -0300

    initial commit

---
+++ src/courses/courses.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { Course } from './entities/course.entity';
+
+@Injectable()
+export class CoursesService {
+  private courses: Course[] = [
+    {
+      id: 1,
+      name: 'Fundamentos NestJS',
+      description: 'Fundamentos NestJS',
+      tags: ['Node.js', 'NestJS', 'Javascript'],
+    },
+  ];
+
+  findAll() {
+    return this.courses;
+  }
+
+  findOne(id: string) {
+    return this.courses.find((course) => course.id === Number(id));
+  }
+
+  create(createCourseDto: any) {
+    this.courses.push(createCourseDto);
+  }
+
+  update(id: string, updateCourseDto: any) {
+    const indexCourse = this.courses.findIndex(
+      (course) => course.id === Number(id),
+    );
+
+    this.courses[indexCourse] = updateCourseDto;
+  }
+
+  delete(id: string) {
+    const indexCourse = this.courses.findIndex(
+      (course) => course.id === Number(id),
+    );
+
+    this.courses.splice(indexCourse, 1);
+  }
+}
