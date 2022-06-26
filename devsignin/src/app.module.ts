deb6d24671ccc432ca81f1ba541969a3f97ab019
Lucas Lira <lucasrocha446@gmail.com>
2022-06-26 00:49:22 -0300

    add jwt strategy

--- devsignin/src/app.module.ts
+++ devsignin/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UsersModule,
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
